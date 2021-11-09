6ea7346b7e6e669fa531a6b3579658ab45d8cc6c
csil-byte <cgscsilva@gmail.com>
2021-11-08 22:12:58 -0300

    commit

--- app/index.php
+++ app/index.php
@@ -1,77 +1,45 @@
 <?php
+// Error Handling
+error_reporting(-1);
+ini_set('display_errors', 1);
 
-use FastRoute\Route;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Factory\AppFactory;
-use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Routing\RouteCollectorProxy;
 use Slim\Routing\RouteContext;
 
 require __DIR__ . '/../vendor/autoload.php';
-require_once './controllers/UsuarioController.php';
+
 require_once './db/AccesoDatos.php';
-require_once 'middlewares/Logger.php';
+// require_once './middlewares/Logger.php';
 
+require_once './controllers/UsuarioController.php';
+
+// Load ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$dotenv->safeLoad();
 
+// Instantiate App
 $app = AppFactory::create();
-$app->setBasePath('/public');
 
-$app->addRoutingMiddleware();
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
+
+// Add parse body
 $app->addBodyParsingMiddleware();
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $request, Response $response, $args) {
-  $response->getBody()->write("hola alumnos de los jueves!");
-  return $response;
-});
-
-// peticiones
+// Routes
 $app->group('/usuarios', function (RouteCollectorProxy $group) {
   $group->get('[/]', \UsuarioController::class . ':TraerTodos');
   $group->get('/{usuario}', \UsuarioController::class . ':TraerUno');
   $group->post('[/]', \UsuarioController::class . ':CargarUno');
-  $group->put('/{usuario}', \UsuarioController::class . ':ModificarUno');
-  $group->delete('/{usuario}', \UsuarioController::class . ':BorrarUno');
-  $group->post('/login', \UsuarioController::class . ':Login');
 });
 
-//Ejercicio 1
-$app->group('/credenciales', function (RouteCollectorProxy $group) {
-  $group->post('[/]', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Metodo POST");
-    return $response;
-  });
-  $group->get('[/]', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Metodo GET");
-    return $response;
-  });
-})->add(\Logger::class . ':VerificadorDeCredenciales');
-
-//Ejercicio 2
-$app->group('/json', function (RouteCollectorProxy $group) {
-  $group->get('[/]', function (Request $request, Response $response, $args) {
-    $json = json_encode(array("mensaje" => "API => GET", "status" => 200));
-    $response->getBody()->write($json);
-    return $response->withHeader('Content-Type', 'application/json');
-  });
-  $group->post('[/]', function (Request $request, Response $response, $args) {
-    $json = json_encode(array("mensaje" => "API => POST", "status" => 200));
-    $response->getBody()->write($json);
-    return $response->withHeader('Content-Type', 'application/json');
-  })->add(\Credenciales::class . ':ValidarPostJSON');
+$app->get('[/]', function (Request $request, Response $response) {
+  $response->getBody()->write("holaaaaaaaaSlim Framework 4 PHP");
+  return $response;
 });
 
-
-
-
-
-$app->group('/login', function (RouteCollectorProxy $group) {
-  $group->post('[/]', \UsuarioController::class . 'ValidarUsuario');
-});
-
-
-// Run app
 $app->run();
